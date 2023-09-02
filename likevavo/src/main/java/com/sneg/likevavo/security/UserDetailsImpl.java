1a3c017705f9b90b2eb2756561c15c97ba92153c
snwm69x <119750978+snwm69x@users.noreply.github.com>
2023-09-02 17:21:21 +0400

    reworked to new db

--- likevavo/src/main/java/com/sneg/likevavo/security/UserDetailsImpl.java
+++ likevavo/src/main/java/com/sneg/likevavo/security/UserDetailsImpl.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
