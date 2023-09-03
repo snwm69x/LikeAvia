7e6ca4c8b73304b30ab8101e492dac49c9cf2750
snwm69x <119750978+snwm69x@users.noreply.github.com>
2023-09-03 21:42:11 +0400

    success initialized BeanTest

--- likevavo/src/test/java/com/sneg/likevavo/LikevavoApplicationTests.java
+++ likevavo/src/test/java/com/sneg/likevavo/LikevavoApplicationTests.java
@@ -1,13 +1,51 @@
 package com.sneg.likevavo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import java.util.List;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.sneg.likevavo.config.BeanTestConfig;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class LikevavoApplicationTests {
 
+	@Autowired
+	private ApplicationContext applicationContext;
+
+	@Autowired
+	private BeanTestConfig beanTestConfig;
+
 	@Test
 	void contextLoads() {
+		List<String> beans = beanTestConfig.returnBeans();
+		int beansSize = beans.size();
+		boolean isOk = false;
+		for (String bean : beans) {
+			if(applicationContext.containsBean(bean)) {
+				beansSize--;
+				System.out.println("Bean " + bean + " working fine");
+			} else {
+				System.out.println("Bean " + bean + " not found");
+			}
+		}
+		if(beansSize == 0) {
+			isOk = true;
+			System.out.println("All beans in context : " + isOk);
+			assert(isOk);
+		} else {
+			assert(isOk);
+		}
+		
 	}
 
 }
